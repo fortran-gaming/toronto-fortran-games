fe5c77412ecc6020e13d47226604a8c345c7f361
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-06-26 22:36:52 -0400

    cmake legacy fortran options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,31 +1,27 @@
-if(CMAKE_BUILD_TYPE STREQUAL Debug)
-  add_compile_options(-g -O0)
-else()
-  add_compile_options(-O3)
-endif()
 
 if(${CMAKE_Fortran_COMPILER_ID} STREQUAL GNU)
-  list(APPEND FFLAGS -Werror=array-bounds -fcheck=all
-  -fexceptions -ffpe-trap=invalid,zero,overflow 
-  #-fimplicit-none
-  -Wline-truncation) #-Warray-temporaries
-  
-  list(APPEND FLAGS -march=native)
+  if(CMAKE_BUILD_TYPE STREQUAL Debug)
+    list(APPEND FFLAGS -Werror=array-bounds -fcheck=all
+    -fexceptions -ffpe-trap=invalid,zero,overflow
+    #-fimplicit-none
+    ) #-Warray-temporaries
+  endif()
+
+  list(APPEND FLAGS -march=native -Wline-truncation)
   list(APPEND CFLAGS -Wall -Wextra -Wpedantic)
-  
-#  link_libraries(-fsanitize=address) Only for Linux, don't bother on CI for now.
-  
+
   if(UNIX AND NOT (APPLE OR CYGWIN))
     add_compile_options(-fstack-protector-all)
   endif()
-  
-  if(${CMAKE_Fortran_COMPILER_VERSION} VERSION_GREATER_EQUAL 8.1)
-    list(APPEND FFLAGS -std=legacy)
-  endif()
-  
+
+  list(APPEND FFLAGS -std=legacy)
+
 elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL Intel)
-  list(APPEND FFLAGS -fpe0 -traceback ) #-implicitnone
-elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL Flang)  # https://github.com/flang-compiler/flang/wiki/Fortran-2008
-  list(APPEND FFLAGS -Mallocatable=03)
+  if(CMAKE_BUILD_TYPE STREQUAL Debug)
+    list(APPEND FFLAGS -fpe0 -traceback) #-implicitnone
+  endif()
+elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL Flang)
+
 elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL PGI)
+
 endif()
