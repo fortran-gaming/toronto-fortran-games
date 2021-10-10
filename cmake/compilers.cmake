3d3770f9f6462a97e8bb61164a663d50a32d241c
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 11:21:22 -0400

    cmake best practices compiler flags

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,5 +1,5 @@
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  string(APPEND CMAKE_Fortran_FLAGS " -w -std=legacy -Wline-truncation")
-  add_compile_options(-mtune=native)
+  add_compile_options(-mtune=native
+    "$<$<COMPILE_LANGUAGE:Fortran>:-w;-std=legacy;-Wline-truncation>")
 endif()
